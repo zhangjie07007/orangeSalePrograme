dcc56a15cc87ad9874d9d50c6aa4aa07c5f2d041
Dich丶 <1591391677@qq.com>
2020-01-29 10:41:09 +0800

    第一次提交小程序代码

---
+++ src/app.jsx
@@ -0,0 +1,73 @@
+import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/mobx'
+import Index from './pages/index'
+
+import counterStore from './store/counter'
+
+import './app.scss'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const store = {
+  counterStore
+}
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index',
+      'pages/mine/index',
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+    "tabBar": {
+      "color": "black",
+      "selectedColor": "white",
+      "borderStyle": "white",
+      "backgroundColor": "white",
+      "list": [
+        {
+          "pagePath": "pages/index/index",
+          "iconPath": "",
+          "selectedIconPath": "",
+          "text": "首页"
+        },
+        {
+          "pagePath": "pages/mine/index",
+          "iconPath": "",
+          "selectedIconPath": "",
+          "text": "我的"
+        }
+      ]
+    },
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,60 @@
+import Taro, {Component} from '@tarojs/taro'
+import {View, Button, Text} from '@tarojs/components'
+import {observer, inject} from '@tarojs/mobx'
+
+import './index.scss'
+
+
+@inject('counterStore')
+@observer
+class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '橙子君'
+  };
+
+  componentWillMount() {
+  }
+
+  componentWillReact() {
+    console.log('componentWillReact')
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  increment = () => {
+    const {counterStore} = this.props;
+    counterStore.increment()
+  };
+
+  decrement = () => {
+    const {counterStore} = this.props;
+    counterStore.decrement()
+  };
+
+  incrementAsync = () => {
+    const {counterStore} = this.props;
+    counterStore.incrementAsync()
+  };
+
+  render() {
+    const {counterStore: {counter}} = this.props;
+    return (
+      <View className='index'>
+        mine
+      </View>
+    )
+  }
+}
+
+export default Index 
